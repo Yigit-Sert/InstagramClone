ca08121c59562b67a02031a3235b4845221f6d04
Yigit-Sert <yigitsert32@gmail.com>
2024-07-22 13:11:16 +0300

    refactor: Add runTransaction to handle like and dislike functionality in Feed component

--- components/main/Feed.js
+++ components/main/Feed.js
@@ -3,7 +3,7 @@ import { StyleSheet, View, FlatList, TouchableOpacity } from "react-native";
 import { Card, Button, Text } from "react-native-paper";
 import { connect } from "react-redux";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
-import { collection, getDocs, query, where, doc, getDoc } from "firebase/firestore";
+import { collection, getDocs, query, where, doc, getDoc, runTransaction } from "firebase/firestore";
 import { db, auth } from "../auth/firebaseConfig";
 import StoryProfile from "./stories/components/StoryProfile";
 import StoryComponent from "./stories/components/StoryComponent";
@@ -67,6 +67,58 @@ function Feed(props) {
 
   const closeStory = () => {
     setSelectedStoryUser(null);
+  };
+
+  const onLikePress = async (uid, postId) => {
+    const currentUserUid = auth.currentUser.uid;
+  
+    try {
+      const likeRef = doc(db, `posts/${uid}/userPosts/${postId}/likes/${currentUserUid}`);
+      const postRef = doc(db, `posts/${uid}/userPosts/${postId}`);
+  
+      await runTransaction(db, async (transaction) => {
+        const postDoc = await transaction.get(postRef);
+        if (!postDoc.exists) {
+          throw 'Post does not exist!';
+        }
+  
+        const currentLikeCounter = postDoc.data().likeCounter || 0;
+        const newLikeCounter = currentLikeCounter + 1;
+  
+        transaction.set(likeRef, {});
+        transaction.update(postRef, { likeCounter: newLikeCounter });
+      });
+  
+      console.log('Like successfully added and likeCounter updated!');
+    } catch (error) {
+      console.error('Error adding like: ', error);
+    }
+  };
+  
+  const onDislikePress = async (uid, postId) => {
+    const currentUserUid = auth.currentUser.uid;
+  
+    const likeRef = doc(db, `posts/${uid}/userPosts/${postId}/likes/${currentUserUid}`);
+    const postRef = doc(db, `posts/${uid}/userPosts/${postId}`);
+  
+    try {
+      await runTransaction(db, async (transaction) => {
+        const postDoc = await transaction.get(postRef);
+        if (!postDoc.exists) {
+          throw 'Post does not exist!';
+        }
+  
+        const currentLikeCounter = postDoc.data().likeCounter || 0;
+        const newLikeCounter = currentLikeCounter - 1;
+  
+        transaction.delete(likeRef);
+        transaction.update(postRef, { likeCounter: newLikeCounter });
+      });
+  
+      console.log('Like successfully removed and likeCounter updated!');
+    } catch (error) {
+      console.error('Error removing like: ', error);
+    }
   };
 
   return (
